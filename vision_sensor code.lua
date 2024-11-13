760624b05f9634ebd40f05fb95c06bcb9c738901
czub <wik.czubek@gmail.com>
2024-11-13 21:13:45 +0100

    Add files via upload

---
+++ vision_sensor code.lua
@@ -0,0 +1,9 @@
+function sysCall_init()
+ sim = require('sim')
+ simVision = require('simVision')
+end
+function sysCall_vision(inData)
+ simVision.sensorImgToWorkImg(inData.handle) -- copy the vision sensor image to the work image
+ simVision.edgeDetectionOnWorkImg(inData.handle, 0.2) -- perform edge detection on the work image
+ simVision.workImgToSensorImg(inData.handle) -- copy the work image to the vision sensor image buffer
+end
